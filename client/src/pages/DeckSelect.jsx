b3bdb4a3758c1d3be54279ce5a967d0c76e481b9
AllredW <allrewil@gmail.com>
2024-09-07 19:46:43 -0400

    updated CSS on DeckSelect page

--- client/src/pages/DeckSelect.jsx
+++ client/src/pages/DeckSelect.jsx
@@ -10,11 +10,24 @@ import {
   Heading,
   Button,
   Text,
+  Box,
 } from "@chakra-ui/react";
 import { QUERY_ME } from "../utils/queries";
-
+import { keyframes } from "@emotion/react";
 import Auth from "../utils/auth";
 import { h2 } from "framer-motion/client";
+
+// Define keyframes for animation
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 const DeckSelect = () => {
   // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
@@ -42,8 +55,10 @@ const DeckSelect = () => {
   }
 
   return (
-    <div>
-      <h1>Hi!</h1>
+    
+    <div className="flex-row justify-center" style={{backgroundColor: "#f7fafc"}}>
+      <h1 style={{display: "flex", justifyContent: "center"}} className="col-12 ">Study Mode</h1>
+      <h4 style={{display: "flex", justifyContent: "center"}} className="col-12 ">Select A Deck</h4>
       {profile.decks?.length > 0 && (
         <div className="deck-list">
           <SimpleGrid spacing={4} templateColumns="repeat(4, 1fr)">
@@ -55,12 +70,13 @@ const DeckSelect = () => {
                   flexDirection="column"
                   justifyContent="center"
                   alignItems="center"
+                  animation={`${fadeIn} 1s ease`}
                 >
                   <CardHeader>
                     <Heading size="md"> {deck.deckName}</Heading>
                   </CardHeader>
                   <CardFooter>
-                    <Button>View here</Button>
+                    <Button>Let's Study!</Button>
                   </CardFooter>
                 </Card>
               </Link>
